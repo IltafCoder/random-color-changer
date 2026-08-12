11982b2f5654d9fb53bb58a581acc51dde64bfaf
Iltaf Khan <138275083+IltafCoder@users.noreply.github.com>
2026-08-12 15:52:22 +0500

    Update Color.jsx

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,1 +1,13 @@
 
+function Color ({c, changeColor}) {
+    return (
+        <>
+            <div className="w-40 h-40 cursor-pointer hover:scale-105 transition duration-100 rounded-md"
+            style={{ backgroundColor: c }} onClick={changeColor}>
+                
+            </div>
+        </>
+    )
+}
+
+export default Color
